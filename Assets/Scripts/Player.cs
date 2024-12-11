9647a2b621e527781488b2f6f1cfd43c18551c06
NaKyoo <axel.letourneur-chiffoleau@edu.esiee-it.fr>
2024-12-11 16:21:58 +0100

    Fix scenes problems

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,38 +10,41 @@ public class Player : MonoBehaviour
     public Transform groundCheck;
     float horizontal;
     SpriteRenderer sr;
+    Animator animator;
 
     private void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() 
     {
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        animator.SetBool("IsGrounded", IsGrounded());
     }
 
-    public void Move(InputAction.CallbackContext context)
+
+    public void Move(InputAction.CallbackContext context) 
     {
+
         horizontal = context.ReadValue<Vector2>().x;
-        if (horizontal < 0)
-            sr.flipX = true;
-        else
-        {
-            sr.flipX = false;
-        }
+        sr.flipX = horizontal < 0 ? true : false;
+
     }
 
-    public void Jump(InputAction.CallbackContext context)
+    public void Jump(InputAction.CallbackContext context) 
     {
         if (context.performed && IsGrounded())
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+            rb.linearVelocity = new Vector2 (rb.linearVelocity.x, jumpingPower);
         }
     }
 
-    bool IsGrounded()
+    bool IsGrounded ()
     {
-        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, .1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.25f, .1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+        
     }
 }
