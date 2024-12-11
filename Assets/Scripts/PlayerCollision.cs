1ffd1fbc8807045e535d8c98f3daab0a733b4afa
Théobinch <theo.lepilliet@edu.esiee-it.fr>
2024-12-11 16:13:43 +0100

    add MORT

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerCollision : MonoBehaviour
+public class PlayerCollisions : MonoBehaviour
 {
-	public int life  = 3;
+	public int life  = 1;
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.CompareTag("Spike"))
+		if (collision.CompareTag("Pique"))
 		{
-			TakeDamages(3);
+			TakeDamages(1);
 		}
 	}
 
 	public void TakeDamages(int damage)
 	{
 		life -= damage;
+		Die();
+	}
+
+	public void Die()
+	{
+		GetComponent<Rigidbody2D>().AddForce(Vector3.up * 150);
+		GetComponent<Collider2D>().isTrigger = true;
+		Invoke("RestartLevel", 0);
+	}
+
+	public void RestartLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
 }
