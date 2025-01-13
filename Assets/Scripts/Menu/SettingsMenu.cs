53ed5bb3f58bd1dd4a1db6d74b58f67c2c882003
Théobinch <theo.lepilliet@edu.esiee-it.fr>
2025-01-13 14:09:30 +0100

    add music menu

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
-{ 
-    public AudioMixer audioMixer;
-    
-    public void SetVolume(float volume)
+{
+    public AudioMixer audioMixer; 
+    public Slider musicSlider;   
+
+    void Start()
     {
-        audioMixer.SetFloat("Volume", volume);
+        float currentVolume;
+        if (audioMixer.GetFloat("MusicVolume", out currentVolume))
+        {
+            musicSlider.value = Mathf.Pow(10, currentVolume / 20);
+        }
+        
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
     }
-    
+
+    public void SetMusicVolume(float sliderValue)
+    {
+        float dbVolume = Mathf.Log10(Mathf.Clamp(sliderValue, 0.0001f, 1f)) * 20;
+        audioMixer.SetFloat("MusicVolume", dbVolume);
+    }
 }
