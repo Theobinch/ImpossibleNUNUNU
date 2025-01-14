a62195b6fc2a01b0274021fc7359236220d7d585
Théobinch <theo.lepilliet@edu.esiee-it.fr>
2025-01-14 14:28:58 +0100

    add resolution

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -5,10 +8,50 @@ using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer; 
-    public Slider musicSlider;   
+    public Slider musicSlider;
+    
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    
+    private Resolution[] resolutions;
+    private List<Resolution> filteredResolutions;
+
+    private float currentRefreshRate;
+    private int currentResolutionIndex = 0;
 
     void Start()
     {
+        // Initialisation des résolutions disponibles et des options
+        resolutions = Screen.resolutions;
+        filteredResolutions = new List<Resolution>();
+        
+        resolutionDropdown.ClearOptions();
+        currentRefreshRate = Screen.currentResolution.refreshRate;
+
+        for (int i = 0; i < resolutions.Length; i++)  // Correction de 'length' en 'Length'
+        {
+            if (resolutions[i].refreshRate == currentRefreshRate)  // Correction de 'cuurerRefreshRate' en 'currentRefreshRate'
+            {
+                filteredResolutions.Add(resolutions[i]);
+            }
+        }
+
+        // Remplir la liste déroulante avec les résolutions filtrées
+        List<string> options = new List<string>();
+        for (int i = 0; i < filteredResolutions.Count; i++)
+        {
+            string resolutionOption = filteredResolutions[i].width + "x" + filteredResolutions[i].height + " ";
+            options.Add(resolutionOption);
+            if (filteredResolutions[i].width == Screen.width && filteredResolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropdown.AddOptions(options); 
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        // Initialiser le volume de la musique
         float currentVolume;
         if (audioMixer.GetFloat("MusicVolume", out currentVolume))
         {
@@ -16,6 +59,15 @@ public class SettingsMenu : MonoBehaviour
         }
         
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = filteredResolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, true);
+        
+        // Afficher la nouvelle résolution dans la console
+        Debug.Log($"Résolution changée à : {resolution.width}x{resolution.height}");
     }
 
     public void SetMusicVolume(float sliderValue)
