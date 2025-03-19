5f05cdba88b19e00eabbca9972cefc5392196b6d
NaKyoo <axel.letourneur-chiffoleau@edu.esiee-it.fr>
2025-03-19 14:46:50 +0100

    fix animation changing level

--- Assets/Scripts/EndLevel/LevelTransition.cs
+++ Assets/Scripts/EndLevel/LevelTransition.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelTransition : MonoBehaviour
 {
     public static LevelTransition instance;
     public Animator animator;
-    public GameObject fadeImageObject;
 
     private void Awake()
     {
@@ -18,13 +18,7 @@ public class LevelTransition : MonoBehaviour
         else
         {
             Destroy(gameObject);
-        }
-    }
-
-    void Start()
-    {
-        FindFadeImage();
-        StartCoroutine(FadeIn());
+        }    
     }
 
     public void LoadSceneWithTransition(string sceneName)
@@ -34,36 +28,9 @@ public class LevelTransition : MonoBehaviour
 
     private IEnumerator PlayTransition(string sceneName)
     {
-         if (animator == null) FindFadeImage();
-        fadeImageObject.SetActive(true);
-        animator.SetTrigger("FadeOutTrigger");
-
-        yield return new WaitForSeconds(1f); 
-
-        SceneManager.LoadScene(sceneName);
-        yield return new WaitForSeconds(0.2f); 
-
-        StartCoroutine(FadeIn());
-    }
-
-    private IEnumerator FadeIn()
-    {
-        animator.SetTrigger("FadeInTrigger");
+        animator.SetTrigger("End");
         yield return new WaitForSeconds(1f);
-        fadeImageObject.SetActive(false); 
-    }
-
-    private void FindFadeImage()
-    {
-        fadeImageObject = GameObject.Find("FadeImage");
-        if (fadeImageObject != null)
-        {
-            animator = fadeImageObject.GetComponent<Animator>();
-            fadeImageObject.SetActive(true);
-        }
-        else
-        {
-            Debug.LogError("FadeImage introuvable dans la scène !");
-        }
+        SceneManager.LoadScene(sceneName);
+        animator.SetTrigger("Start");
     }
 }
